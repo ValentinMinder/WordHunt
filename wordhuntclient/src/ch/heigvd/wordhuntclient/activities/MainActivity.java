de0aa6012740f1daa41df73639fc0335b415764b
ValentinMinder <ValentinMinder@users.noreply.github.com>
2015-05-05 10:05:27 +0200

    button for ping test with text field for IP...

--- wordhuntclient/src/ch/heigvd/wordhuntclient/activities/MainActivity.java
+++ wordhuntclient/src/ch/heigvd/wordhuntclient/activities/MainActivity.java
@@ -7,20 +7,29 @@ import protocol.Protocol.Request;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import ch.heigvd.gen.wordhuntclient.R;
 import ch.heigvd.wordhuntclient.asynctask.WordHuntASyncTask;
 
 public class MainActivity extends Activity {
-	
+
 	WordHuntASyncTask iotask;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// auto-launch
-		testPing();
+
+		final Button b = (Button) findViewById(R.id.button_test_ping);
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				b.setText(testPing());
+			}
+		});
 	}
 
 	@Override
@@ -29,9 +38,14 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	private void testPing () {
-		String host = "192.168.0.18"; //"10.192.94.246";
+
+	private String testPing() {
+		String host = "10.192.94.186"; // "10.192.94.246";
+		final Button b = (Button) findViewById(R.id.button_test_ping);
+		String t = b.getText().toString();
+		if (t != null && !t.equals("")) {
+			host = t;
+		}
 		int port = 1234;
 		WordHuntASyncTask iotask = new WordHuntASyncTask(host, port);
 		System.out.println("Scheduling future iotask.");
@@ -41,6 +55,7 @@ public class MainActivity extends Activity {
 			// blocking on response.
 			Object received = iotask.get();
 			System.out.println("Receiving back in GUI: " + received);
+			return received.toString();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -48,5 +63,6 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return "error.";
 	}
 }
