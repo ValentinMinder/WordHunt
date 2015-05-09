59a6a9620696eccfea993daa53e15acc13e411ea
David Kunzmann <davidkunzmann@gmail.com>
2015-05-09 14:28:54 +0200

    Added: getScore form Score object, getLetterOccurences and Language enum

---
+++ wordhuntshared/src/whobjects/LetterOccurences.java
@@ -0,0 +1,59 @@
+package whobjects;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by David on 09.05.2015.
+ */
+public class LetterOccurences {
+
+    private double[] languageOccurences;
+
+
+
+    private static final int nbOfLetters = 26;
+    public static enum language {FR, EN};
+    private String OccurenceFile ; //here we set the languages
+    private static LetterOccurences instance;
+
+    private LetterOccurences(language lang){
+        switch (lang){
+            case EN:
+                OccurenceFile = "wordhuntshared/englishOccurencesToParse.txt";
+            default:
+                OccurenceFile = "wordhuntshared/frenchOccurencesToParse.txt";
+        }
+
+    }
+
+    public static LetterOccurences getInstance(language lang){
+        if(instance == null){
+            instance = new LetterOccurences(lang);
+        }
+        return instance;
+    }
+
+    public double[] getLanguageOccurences(){
+        languageOccurences = new double[nbOfLetters];
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(OccurenceFile)));
+            System.out.println();
+            for (int i = 0; i < nbOfLetters; i++) {
+                languageOccurences[i] = Double.valueOf(br.readLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return languageOccurences;
+    }
+    public static int getNbOfLetters() {
+        return nbOfLetters;
+    }
+
+}
---
+++ wordhuntshared/src/whobjects/Score.java
@@ -0,0 +1,38 @@
+package whobjects;
+
+/**
+ * Created by David on 09.05.2015.
+ */
+public class Score {
+
+    private int score;
+    private double[] languageOccurences;
+    private Score(){
+        languageOccurences = new double[LetterOccurences.getNbOfLetters()];
+    }
+
+    public int getScore(String word){
+        score = 0;
+        languageOccurences = LetterOccurences.getInstance(LetterOccurences.language.FR).getLanguageOccurences();
+        score += (word.length() - 2) * 100; //Score start at 100 for a 3 letters word
+        for(int i = 0; i < word.length(); i++){ // More points if the letter occurs less in language
+            char c = word.charAt(i);
+            double letterWeight = 1/languageOccurences[c - 'A'];
+//            System.out.println("letterWeight: " + letterWeight);
+            score += (letterWeight/4)*100;
+        }
+
+        return score;
+    }
+
+    public static void main(String[] args) {
+        Score test = new Score();
+//        System.out.printf("Score : Tatamis :" + test.getScore("TATAMIS"));
+//        System.out.printf("Score : ZYGOTE :" + test.getScore("ZYGOTE"));
+
+    }
+
+
+
+
+}
