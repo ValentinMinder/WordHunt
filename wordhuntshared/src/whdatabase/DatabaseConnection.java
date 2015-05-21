f3326d8066ab4e63fe2530797da249d92e253e9b
David Kunzmann <davidkunzmann@gmail.com>
2015-05-21 21:26:12 +0200

    Database Connection Ladies ;)

--- wordhuntshared/src/whdatabase/DatabaseConnection.java
+++ wordhuntshared/src/whdatabase/DatabaseConnection.java
@@ -1,7 +1,50 @@
 package whdatabase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by David on 11.05.2015.
  */
 public class DatabaseConnection {
+
+
+    public static DatabaseConnection dbinstance;
+    private Connection conn;
+    private String dbName;
+    private String port;
+    private String user;
+    private String pwd;
+
+    private DatabaseConnection() {
+
+        user = "root";
+        pwd = "";
+        dbName = "test";
+        port = "3306";
+
+
+    }
+
+    public static DatabaseConnection getInstance(){
+        if(dbinstance == null){
+            dbinstance = new DatabaseConnection();
+        }
+
+        return dbinstance;
+    }
+
+    public Connection getConnection() {
+
+        try {
+            conn=DriverManager.getConnection( "jdbc:mysql://localhost:" + port +"/" + dbName + "?user=" +user + "&password=" + pwd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return conn;
+    }
+
 }
