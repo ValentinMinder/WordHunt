0894c9da4d07ccb85349b6eca5cd49e010525da9
David Kunzmann <davidkunzmann@gmail.com>
2015-05-22 16:40:08 +0200

    Verify is registered, control of credentials

--- wordhuntshared/src/whdatabase/User.java
+++ wordhuntshared/src/whdatabase/User.java
@@ -10,19 +10,43 @@ import java.sql.SQLException;
  */
 public class User {
 
+    private String name;
+    private String email;
 
+    public String getPassword() {
+        return password;
+    }
 
-    public void registerUser(String nom , String email, String password){
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private String password;
+
+    public User(String name , String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void registerUser(){
         Connection conn = DatabaseConnection.getInstance().getConnection();
         PreparedStatement stmt = null;
 
         try {
-            stmt = conn.prepareStatement("INSERT INTO utilisateur (nom_utilisateur, email, mot_de_passe) VALUES (?,?,SHA(?)) ");
-            stmt.setString(1, nom);
-            stmt.setString(2, email);
-            stmt.setString(3, password);
-            stmt.executeUpdate();
-            System.out.println("Something");
+            if(!isAlreadyRegistered()) {
+                stmt = conn.prepareStatement("INSERT INTO utilisateur (nom_utilisateur, email, mot_de_passe) VALUES (?,?,SHA(?)) ");
+                stmt.setString(1, name);
+                stmt.setString(2, email);
+                stmt.setString(3, password);
+                stmt.executeUpdate();
+            }else{
+                System.out.println("Already registered!");
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -30,7 +54,31 @@ public class User {
 
     }
 
-    public void verifyCredentials(){
+    public boolean correctCredentials(){
+        Connection conn = DatabaseConnection.getInstance().getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            if(isAlreadyRegistered()){
+                stmt = conn.prepareStatement("SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = SHA(?)");
+                stmt.setString(1, email);
+                stmt.setString(2, password);
+                rs = stmt.executeQuery();
+                if(!rs.isBeforeFirst()){
+                    //Wrong Credentials
+                    return false;
+                }
+                return true;
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //Not Registered
+        return false;
+
 
     }
 
@@ -39,7 +87,7 @@ public class User {
 
     }
 
-    public boolean isAlreadyRegistered(String email){
+    public boolean isAlreadyRegistered(){
         Connection conn = DatabaseConnection.getInstance().getConnection();
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -62,12 +110,21 @@ public class User {
 
 
     public static void main(String[] args) {
-        User usr = new User();
-        usr.registerUser("David", "david@f.com", "othersecret");
+        User usr = new User("David", "david@f.com", "othersecret");
+        usr.registerUser();
 
-        if(usr.isAlreadyRegistered("david@f.com")){
-            System.out.println("Already registered");
+        if(usr.isAlreadyRegistered()){
+            System.out.println("Already registered here");
         }
+
+        User me = new User("Jean", "david@f.com", "othersecret");
+        me.registerUser();
+        if(me.correctCredentials()){
+            System.out.println("OK You're logged");
+        }else{
+            System.out.println("Nope go away!!!");
+        }
+
 
 
 
